196e83fc00590b98cc588e03b6d731c179f096ca
User <User@localhost.localdomain>
2018-05-14 11:45:58 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,6 +3,28 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+var flipCard = function(cardId) {
+	card = cards[cardId];
+	cardsInPlay.push(card);
+	console.log("User flipped " + card);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+/*
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped " + cardsInPlay[0]);
@@ -18,3 +40,4 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry, try again.");
 	}
 }
+*/
